8a9ef385c6b1203375ef60b67d27459de1ed2fe6
notemakerabq <mbarker@paloaltonetworks.com>
2020-05-08 11:14:09 -0600

    added web-analytics

---
+++ notemaker-demo/web-analytics/deploy/mysql-queries/setup.sql
@@ -0,0 +1,8 @@
+
+CREATE USER 'admin' IDENTIFIED BY 'PrismaIsSecure5xuv';
+
+GRANT USAGE ON *.* TO 'admin'@'%' IDENTIFIED BY 'PrismaIsSecure5xuv'; 
+
+GRANT ALL privileges ON *.* TO 'admin'@'%';
+
+
