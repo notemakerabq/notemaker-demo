596ee404a4a271372f5824b57c1d70118817c5b4
notemakerabq <mattdevsecops@outlook.com>
2020-06-16 14:07:53 -0600

    removed web-analytics, added nginx

--- web-analytics/deploy/test/mysql-queries/setup.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE USER 'admin' IDENTIFIED BY 'PrismaIsSecure5xuv';
-
-GRANT USAGE ON *.* TO 'admin'@'%' IDENTIFIED BY 'PrismaIsSecure5xuv'; 
-
-GRANT ALL privileges ON *.* TO 'admin'@'%';
-
-
